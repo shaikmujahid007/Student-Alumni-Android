dc2b4f2e913d6aa568b36d3c425684b776597cb5
sudheer_ranga <67502240+sudheerranga0512@users.noreply.github.com>
2020-08-21 00:26:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,4 +18,25 @@ x=x+data+"</br>"
 
 http.listen(3000, function() {
    console.log('listening on localhost:3000');
-}); 
+});  
+//by using your own room id code you can create room and start group chat there 
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);  
+let x=[];
+app.get('/', function(req, res) {
+   res.sendfile('index.html');
+});
+
+io.on('connection', function(socket) {
+   socket.on('clientEvent', function(data) { 
+socket.join("room-"+data.code);  
+if(x[data.code]==undefined){
+   x[data.code]="";
+}
+x[data.code]=x[data.code]+data.message+"</br>"; 
+
+   io.sockets.in("room-"+data.code).emit('connectToRoom', {description:x[data.code]});
+     
+});
+
